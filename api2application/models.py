d6680972c40874c24d6e7644e8c07f45e26c9d5d
SyHoMadara <sh.sarafrazi80@gmail.com>
2021-07-12 14:15:03 +0430

    creation api models complete

--- api2application/models.py
+++ api2application/models.py
@@ -1,28 +1,47 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
 
 class UserManager(BaseUserManager):
     def creat_user(self, email, first_name, last_name, password=None):
         if not email:
-            return ValueError("User must have email address")
+            raise ValueError("User must have email address")
         elif not (first_name and last_name):
-            return ValueError("First name and Last name required")
+            raise ValueError("First name and Last name required")
         user = self.model(
-
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name
         )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def creat_superuser(self, email, first_name, last_name, password=None):
+        user = self.creat_user(
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            password=password
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_supperuser = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser):
+    # user properties
     email = models.EmailField(verbose_name='email', max_length=60, unique=True)
-    # username = models.CharField(max_length=30, unique=True)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=14, null=True, blank=False)
     date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
     last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
+    # picture = models.ImageField()
 
     # permutations
     is_admin = models.BooleanField(default=False)
@@ -43,90 +62,23 @@ class User(AbstractBaseUser):
         return True
 
 
-# # users
-# class User(models.Model):
-#     USER_TYPE = (
-#         (0, 'SUPER_ADMIN'),
-#         (1, 'NORMAL_ADMIN'),
-#         (2, 'NORMAL_USER')
-#     )
-#
-#     USER_RATE = (
-#         (0, "Newbie"),
-#         (1, "Pupil"),
-#         (2, "Specialist"),
-#         (3, "Expert"),
-#         (4, "CandiDate Master"),
-#         (5, "Master"),
-#         (6, "Grand Master"),
-#         (7, "International Grand Master"),
-#         (8, "International Legendary Grand Master"),
-#         (9, "Admin"),
-#         (10, "Creator")
-#     )
-#
-#     email_address = models.CharField(max_length=60, blank=False)
-#     password = models.CharField(max_length=60, blank=False)
-#     first_name = models.CharField(max_length=30, blank=False)
-#     last_name = models.CharField(max_length=30, blank=False)
-#     phone_number = models.CharField(max_length=15, null=True, blank=False)
-#
-#     user_type = models.SmallIntegerField(choices=USER_TYPE, default=2)
-#     user_rate = models.SmallIntegerField(choices=USER_RATE, default=0)
-#
-#     def __str__(self):
-#         return str(self.id) + "." + self.email_address
 
 
 class Product(models.Model):
     title = models.CharField(max_length=50, blank=False)
     cost = models.DecimalField(decimal_places=0, max_digits=12)
     rate = models.IntegerField(default=0)  # between 0 and 5.
-    # user = models.ForeignKey('api2application.models.Employee', null=True, blank=True, on_delete=models.CASCADE)
+    user = models.ForeignKey('User', blank=True, on_delete=models.CASCADE)
     description = models.CharField(max_length=400, default="description", blank=True)
     product_category = models.ForeignKey('ProductCategory', related_name="Products", null=True,
                                          on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title  # + " : " + str(self.user.id) + "." + self.user.email_address
+        return self.title + " : " + self.user.id.__str__() + "." + self.user.email.__str__()
 
 
-# DIGITAL_TYPE = (
-#     (-1, "Another"),
-#     (0, "Phones"),
-#     (1, "Laptop"),
-#     (3, "Accessories"),
-#     (4, "TV"),
-# )
-#
-# HOME_APPLICATION_TYPE = (
-#     (-1, "Another"),
-#     (0, "Washing Machine"),
-#     (1, "Dishwasher"),
-#     (2, "Refrigerators"),
-#     (3, "Vacuum Cleaners"),
-#     (4, "Kitchen appliances")  # like dishes , fork , etc.
-# )
-#
-# VEHICLE_TYPE = (
-#     (-1, "Another"),
-#     (0, "Automobile"),
-#     (1, "Motorcycle"),
-#     (2, "Boat and Tools")
-# )
-#
-# GENERAL_PRODUCT_TYPE = (
-#     (-1, "Another"),
-#     (0, DIGITAL_TYPE),
-#     (1, "Builds"),
-#     (2, HOME_APPLICATION_TYPE),
-#     (3, VEHICLE_TYPE),
-#     (4, "Self Tools"),
-#
-# )
 class ProductCategory(MPTTModel):
     name = models.CharField(max_length=200)
-    # slug = models.SlugField(unique=True, allow_unicode=True, blank=True)
     parent = TreeForeignKey(
         'self',
         blank=True,
@@ -136,7 +88,6 @@ class ProductCategory(MPTTModel):
     )
 
     class Meta:
-        # unique_together = ('slug', 'parent',)
         verbose_name_plural = "Product Categories"
 
     def __str__(self):
@@ -151,10 +102,3 @@ class ProductCategory(MPTTModel):
 
 class Pictures(models.Model):
     product = models.OneToOneField('Product', on_delete=models.CASCADE)
-
-    # def get_all_picture_address(self):
-
-
-class Picture(models.Model):
-    name = models.CharField(max_length=25, blank=False)
-    # image = models.ImageField(nam)
