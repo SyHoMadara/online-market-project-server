ac6f47183ab5353cc9210f3ed4d4b281d807bb8b
SyHoMadara <sh.sarafrazi80@gmail.com>
2021-07-15 13:42:46 +0430

    api urls

---
+++ account/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import re_path
+from .views import *
+
+from handlers.validators import EMAIL_REGEX
+
+app_name = 'account'
+urlpatterns = [
+    re_path(r'(?P<email>' + EMAIL_REGEX + ')/', account_view, name='user api'),
+]
