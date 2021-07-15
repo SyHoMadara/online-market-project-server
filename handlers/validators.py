a4d8e6d63e6ae835a1f7ed55d3e8e78a4f18a0ca
SyHoMadara <sh.sarafrazi80@gmail.com>
2021-07-15 13:11:18 +0430

    adding handlers

---
+++ handlers/validators.py
@@ -0,0 +1,6 @@
+import re
+from django.contrib.auth.validators import *
+from django.contrib.auth.password_validation import *
+EMAIL_REGEX = '[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}'
+PASSWORD_REGEX = ''
+
